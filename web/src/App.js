b1b5cfb6237035e03060c8c14f32f1cdf26f0a4e
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-04-08 18:52:18 -0300

    feat(web): update app.js

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import Header from './Components/Layout/Header'
+import Home from './Components/Pages/Home'
+import './styles/app.css'
 function App() {
   return (
     <div className="App">
-      Hello world
+      <Header />
+      <Home/>
     </div>
   );
 }
