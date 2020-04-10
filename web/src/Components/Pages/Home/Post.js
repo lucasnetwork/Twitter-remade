aff54449de1d9ed158400aaac835765a789b17e7
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-04-10 16:14:19 -0300

    feat(web): add Post in timeline

---
+++ web/src/Components/Pages/Home/Post.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../../../styles/post.css'
+export default function Post(){
+    return(
+        <article className="post-individual">
+             <img src="https://pbs.twimg.com/profile_images/1195485835849879553/z8rcWtDZ_400x400.jpg"/>
+            <div className="text">
+                <p>LucafdsafdsfsdfadsfasdfsadfdsafsadfsdafsdafsdafsLucafdsafdsfsdfadsfasdfsadfdsafsadfsdafsdafsdafsLucafdsafdsfsdfadsfasdfsadfdsafsadfsdafsdafsdafsLucafdsafdsfsdfadsfasdfsadfdsafsadfsdafsdafsdafsLucafdsafdsfsdfadsfasdfsadfdsafsadfsdafsdafsdafsLucafdsafdsfsdfadsfasdfsadfdsafsadfsdafsdafsdafsLucafdsafdsfsdfadsfasdfsadfdsafsadfsdafsdafsdafsLucafdsafdsfsdfadsfasdfsadfdsafsadfsdafsdafsdafsLucafdsafdsfsdfadsfasdfsadfdsafsadfsdafsdafsdafs</p>
+                <img/>
+            </div>
+        </article>
+    )
+}
+                // <div className="">
+                //     <div>
+                //         <img/>
+                //         <h2>post</h2>
+                //     </div>
+                // </div>
