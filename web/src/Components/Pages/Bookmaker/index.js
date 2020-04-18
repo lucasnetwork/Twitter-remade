fafa7e21b29e53b12ad1acbf0ceebe6d73b019e3
Lucas Ribeiro <Lucas Santos Ribeiro>
2020-04-18 14:06:32 -0300

    feat(web): add bookmaker

---
+++ web/src/Components/Pages/Bookmaker/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Header'
+import Trends from '../Home/Trends'
+import Post from '../../Layout/Post'
+
+export default function BookMark(){
+    return(
+        <main>
+            <div>
+                <Header/>
+                <Post imagePerson="https://pbs.twimg.com/profile_images/1195485835849879553/z8rcWtDZ_400x400.jpg" image="qualquercoisa" text="ola mundo" existPostLink={true}/>
+                <Post imagePerson="https://pbs.twimg.com/profile_images/1195485835849879553/z8rcWtDZ_400x400.jpg" image="qualquercoisa" text="Testando posts"/>
+                <Post imagePerson="https://pbs.twimg.com/profile_images/1195485835849879553/z8rcWtDZ_400x400.jpg" image="qualquercoisa" text="ola mundo"/>
+            </div>
+            <Trends/>
+        </main>
+    )
+}
