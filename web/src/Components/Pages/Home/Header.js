ff85048bae3feb4528322df716390746ad55a64e
Lucas Ribeiro <Lucas Santos Ribeiro>
2020-04-18 14:07:12 -0300

    feat(web): add header of home page

---
+++ web/src/Components/Pages/Home/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {FaPen} from 'react-icons/fa'
+export default function Header(){
+    return(
+        <div className="header-home">
+            <h1>Home</h1>
+            <FaPen/>
+        </div>
+    )
+}
