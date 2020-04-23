c6fdf69d37af2680e7f114be9957993162098c98
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-04-23 15:04:20 -0300

    feat(web): the Component createPost works

--- web/src/Components/Pages/Home/CreatePost.js
+++ web/src/Components/Pages/Home/CreatePost.js
@@ -1,18 +1,28 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {FaImage,FaChartBar} from 'react-icons/fa'
 import '../../../styles/createPost.css'
-export default function(){
+export default function({posts,set}){
+    const [text,setText] = useState("")
+    function handlerPost(e){
+        e.preventDefault()
+        set([...posts,
+            {
+                imagePerson:"https://pbs.twimg.com/profile_images/1195485835849879553/z8rcWtDZ_400x400.jpg",
+                text
+            }])
+        setText("")
+    }
     return(
         <article className="post">
             <img alt="foto da pessoa"src="https://pbs.twimg.com/profile_images/1195485835849879553/z8rcWtDZ_400x400.jpg"/>
-            <div class="text">
-                <textarea placeholder="no Que esta pensando"/>
+            <div className="text">
+                <textarea onKeyDown={(e) =>(e.key === "Enter")? handlerPost(e): ""} value={text} onChange={(e) => setText(e.target.value)} placeholder="no Que esta pensando"/>
                 <div className="button-actions">
                     <div>
                         <FaImage/>
                         <FaChartBar/>
                     </div>
-                    <button>Tweet</button>
+                    <button onClick={handlerPost}>Tweet</button>
                 </div>
             </div>
         </article>
--- web/src/Components/Pages/Home/index.js
+++ web/src/Components/Pages/Home/index.js
@@ -5,24 +5,15 @@ import Trends from './Trends'
 import Header from './Header'
 import '../../../styles/main.css'
 export default function Home(){
-    const [posts] = useState([{
-            imagePerson:"https://pbs.twimg.com/profile_images/1195485835849879553/z8rcWtDZ_400x400.jpg",
-            text:"Primeiro texto, testando frontend"
-        },
-        {
-            imagePerson:"https://pbs.twimg.com/profile_images/1195485835849879553/z8rcWtDZ_400x400.jpg",
-            text:"testando",
-            img:"https://i.ytimg.com/vi/ePw190n93Dg/maxresdefault.jpg"
-        }
-])
+    const [posts,setPosts] = useState([])
     return(
         <>
             <main>
                 <div>
                     <Header/>
-                    <CreatePost/>
-                    {posts.map(post =>(
-                        <Post imagePerson={post.imagePerson} image={post.img} text={post.text} urlPostLink={post.link}/>
+                    <CreatePost posts={posts} set={setPosts}/>
+                    {posts.map((post,id) =>(
+                        <Post key={id} imagePerson={post.imagePerson} image={post.img} text={post.text} urlPostLink={post.link}/>
                     ))}
                 </div>
                 <Trends/>
