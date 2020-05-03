22ac4cea9f48ec4bc112254c121e79d349e99438
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-05-02 21:37:04 -0300

    feat(web): implemente styles components

--- web/src/Components/Pages/Home/CreatePost.js
+++ web/src/Components/Pages/Home/CreatePost.js
@@ -2,6 +2,7 @@ import React,{useState} from 'react'
 import {FaImage,FaChartBar} from 'react-icons/fa'
 import {ButtonTweet} from '../../../styles/Buttons'
 import {ImagePerson} from '../../../styles/ImageTags'
+import {PostContent,TextContent} from '../../../styles/Text'
 import '../../../styles/createPost.css'
 export default function({posts,set}){
     const [text,setText] = useState("")
@@ -15,9 +16,9 @@ export default function({posts,set}){
         setText("")
     }
     return(
-        <article className="post">
+        <PostContent >
             <ImagePerson height="70px" alt="foto da pessoa"src="https://pbs.twimg.com/profile_images/1195485835849879553/z8rcWtDZ_400x400.jpg"/>
-            <div className="text">
+            <TextContent className="text">
                 <textarea onKeyDown={(e) =>(e.key === "Enter")? handlerPost(e): ""} value={text} onChange={(e) => setText(e.target.value)} placeholder="no Que esta pensando"/>
                 <div className="button-actions">
                     <div>
@@ -26,7 +27,7 @@ export default function({posts,set}){
                     </div>
                     <ButtonTweet onClick={handlerPost}>Tweet</ButtonTweet>
                 </div>
-            </div>
-        </article>
+            </TextContent>
+        </PostContent>
     )
 }
